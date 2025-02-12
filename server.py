f538a14a206bb9e7b47147840fc1f0b3196b62cc
Kimberly McCarty <kimberly.mccarty@onepeloton.com>
2025-02-12 15:11:27 -0500

    Added 'say' command to impl

--- server.py
+++ server.py
@@ -7,12 +7,20 @@
 
 from mcp.server.fastmcp import FastMCP
 import datetime
+import subprocess
+import sys
 
 mcp = FastMCP("Honk TTS")
 
 @mcp.tool()
+def say_message(message: str) -> dict:
+    """Use the MacOS tts to say the provided message"""
+    subprocess.run(["say", message])
+    return {'message': message, 'status': 'spoken'}
+
+@mcp.tool()
 def getDateTime() -> dict:
-    """Use the MacOS tts to say things"""
+    """Get the current date and time"""
     return {'datetime': datetime.datetime.now() }
 
 def main():
